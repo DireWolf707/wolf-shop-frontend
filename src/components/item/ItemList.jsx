c83d684eadd8dcf9c7e92586e8d959e90764cef9
DireWolf707 <rahulsood707@gmail.com>
2023-05-15 22:29:16 +0530

    added item list component

---
+++ src/components/item/ItemList.jsx
@@ -0,0 +1,37 @@
+import { Stack, Box, Typography } from "@mui/material"
+import BuyNowButton from "./BuyNowButton"
+import AddToCartButton from "./AddToCartButton"
+
+const ItemList = ({ items }) => {
+  return (
+    <Stack flexDirection="row" flexWrap="wrap" justifyContent="center" alignItems="strech" gap={2} p="20px">
+      {items.map((item) => (
+        <Stack key={item.id} width="300px" bgcolor="#fff" borderRadius="14px">
+          <Box component="img" src={item.thumbnail} height="240px" borderBottom="4px solid #000"  borderRadius="12px" m="9px" sx={{ objectFit: "cover" }} />
+
+          <Stack m="6px 14px 14px 14px">
+            <Typography color="#000" fontFamily="Righteous" fontSize="24px">
+              {item.name}
+            </Typography>
+            <Typography color="#000" fontFamily="Alkatra" fontSize="16px">
+              {item.brand}
+            </Typography>
+          </Stack>
+
+          <Stack mt="auto" p="0 6px 6px 6px" bgcolor="rgba(0,0,0,0.1)">
+            <Typography color="#000" fontFamily="Righteous" fontSize="20px" textAlign="center" mt="14px" mb="8px">
+              Rs. {item.price}
+            </Typography>
+
+            <Stack flexDirection="row" gap={1} p="4px">
+              <BuyNowButton item={item} />
+              <AddToCartButton item={item} />
+            </Stack>
+          </Stack>
+        </Stack>
+      ))}
+    </Stack>
+  )
+}
+
+export default ItemList
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react"
 import { Stack } from "@mui/material"
+import { itemApi } from "../store"
+import ItemList from "../components/item/ItemList"
+import requestHandler from "../utils/requestHandler"
 
 const Shop = () => {
-  return <Stack flexGrow={1}>Shop</Stack>
+  const [fetchItems, { data }] = itemApi.useLazyGetItemListQuery()
+
+  useEffect(() => {
+    requestHandler(fetchItems().unwrap(), "fetching products", "products fetched")
+  }, [])
+
+  return (
+    <Stack flexGrow={1}>
+      {data && <ItemList items={data.data} />}
+      {!data && "loading skeleton..."}
+    </Stack>
+  )
 }
 
 export default Shop
