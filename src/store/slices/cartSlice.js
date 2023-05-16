5a516919812ac480b5a56a4d5e8450f54692b6ca
DireWolf707 <rahulsood707@gmail.com>
2023-05-16 17:16:03 +0530

    added add/remove item reducers

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -5,11 +5,30 @@ export const cartSlice = createSlice({
 
   initialState: {
     isCartOpen: false,
+    cart: [],
   },
 
   reducers: {
     toggleCart(state, action) {
       state.isCartOpen = action.payload
+    },
+
+    addItem(state, action) {
+      const newItem = action.payload
+      const prevItem = state.cart.find((item) => item.id === newItem.id)
+
+      if (prevItem) prevItem.qty++
+      else state.cart.append({ ...newItem, qty: 1 })
+    },
+
+    removeItem(state, action) {
+      state.cart.filter((item) => {
+        if (item.id !== action.payload) return true
+        if (item.qty <= 1) return false
+
+        item.qty++
+        return true
+      })
     },
   },
 })
