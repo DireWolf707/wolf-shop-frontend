386e8933932d2aef02c28070abea565c53365532
DireWolf707 <rahulsood707@gmail.com>
2023-05-16 14:54:32 +0530

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 import Navbar from "./components/layout/Navbar"
 import Sidebar from "./components/layout/Sidebar"
 import LoggedInRoute from "./components/wrapper/LoggedInRoute"
-import SocketProvider from "./components/wrapper/SocketProvider"
+// import SocketProvider from "./components/wrapper/SocketProvider"
 import { Stack } from "@mui/material"
 import { Routes, Route } from "react-router-dom"
 import { Home, Profile, Shop, Error404, Error500 } from "./pages"
-import { userApi } from "./store"
 
 const App = () => {
-  const { data: profile } = userApi.useFetchProfileQuery()
-  const user = profile.data
-
   return (
     <Stack height="100vh" width="100vw" overflow="auto" bgcolor="#191924">
       <Navbar />
-      {user && <Sidebar />}
+
+      <Sidebar />
 
       <Routes>
         {/* Public Routes */}
+
+        {/* Socket Disabled Routes */}
         <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+
+        {/* Socket Enabled Routes */}
+        {/* <Route element={<SocketProvider />}></Route> */}
+
         {/* LoggedIn Routes */}
         <Route element={<LoggedInRoute />}>
-          {/* Socket Enabled Routes */}
-          <Route element={<SocketProvider />}></Route>
           {/* Socket Disabled Routes */}
-          <Route path="/shop" element={<Shop />} />
           <Route path="/profile" element={<Profile />} />
+
+          {/* Socket Enabled Routes */}
+          {/* <Route element={<SocketProvider />}></Route> */}
         </Route>
+
         {/* Server Error (500) */}
         <Route path="/500" element={<Error500 />} />
+
         {/* Unknown Routes (404) */}
         <Route path="*" element={<Error404 />} />
         {/* End */}
