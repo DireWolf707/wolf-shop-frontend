7adbea33400ecc6840e2930c2233b422a7e4588d
DireWolf707 <rahulsood707@gmail.com>
2023-05-16 22:59:59 +0530

    added cart badge

--- src/components/cart/CartButton.jsx
+++ src/components/cart/CartButton.jsx
@@ -1,9 +1,10 @@
 import { Stack, Button, Badge, Typography } from "@mui/material"
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
-import { useDispatch, cartSliceActions } from "../../store"
+import { useSelector, useDispatch, cartSliceActions } from "../../store"
 
 const CartButton = ({ gap = 0, textTransform = "lowercase", closeSidebar = null }) => {
   const dispatch = useDispatch()
+  const { cart } = useSelector((store) => store.cart)
 
   const openCartHandler = () => {
     if (closeSidebar) closeSidebar()
@@ -13,7 +14,14 @@ const CartButton = ({ gap = 0, textTransform = "lowercase", closeSidebar = null 
   return (
     <Button onClick={openCartHandler}>
       <Stack flexDirection="row" alignItems="center" gap={gap}>
-        <ShoppingCartIcon />
+        <Badge
+          badgeContent={cart.reduce((acc, item) => acc + item.qty, 0)}
+          max={99}
+          color="primary"
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        >
+          <ShoppingCartIcon />
+        </Badge>
 
         <Typography fontFamily="Righteous" fontSize="14px" textTransform={textTransform}>
           Cart
