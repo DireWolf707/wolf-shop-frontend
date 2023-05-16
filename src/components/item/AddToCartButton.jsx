a7f2d4bc33ea8bf39ca1811cc7f2da9591ec2a3d
DireWolf707 <rahulsood707@gmail.com>
2023-05-16 11:46:26 +0530

    added add to cart button

---
+++ src/components/item/AddToCartButton.jsx
@@ -0,0 +1,7 @@
+import { Button } from "@mui/material"
+
+const AddToCartButton = ({ itemId }) => {
+  return <Button variant="contained" color="primary" fullWidth>Add To Cart</Button>
+}
+
+export default AddToCartButton
