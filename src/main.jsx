92fa34fecb454e931dbd27636083e6bb33a82005
DireWolf707 <rahulsood707@gmail.com>
2023-05-19 00:05:12 +0530

    added cart slice persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,25 @@ import App from "./App.jsx"
 import AuthCheck from "./components/wrapper/AuthCheck"
 import { ThemeProvider } from "@mui/material"
 import { Provider as StoreProvider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
 import { BrowserRouter as Router } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
-import { store } from "./store"
+import { store, persistor } from "./store"
 import { theme } from "./utils/theme.js"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StoreProvider store={store}>
-    <ThemeProvider theme={theme}>
-      <Toaster />
+    <PersistGate persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <Toaster />
 
-      <AuthCheck>
-        <Router>
-          <App />
-        </Router>
-      </AuthCheck>
-    </ThemeProvider>
+        <AuthCheck>
+          <Router>
+            <App />
+          </Router>
+        </AuthCheck>
+      </ThemeProvider>
+    </PersistGate>
   </StoreProvider>
 )
--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 import { dataSlice, dataSliceActions } from "./slices/dataSlice"
 import { checkoutSlice, checkoutSliceActions } from "./slices/checkoutSlice"
 import { cartSlice, cartSliceActions } from "./slices/cartSlice"
 import userApi from "./apis/userApi"
 import itemApi from "./apis/itemApi"
 
-export const store = configureStore({
-  reducer: {
-    // slices
-    [dataSlice.name]: dataSlice.reducer,
-    [checkoutSlice.name]: checkoutSlice.reducer,
-    [cartSlice.name]: cartSlice.reducer,
-    // apis
-    [userApi.reducerPath]: userApi.reducer,
-    [itemApi.reducerPath]: itemApi.reducer,
+const rootReducer = combineReducers({
+  // slices
+  [dataSlice.name]: dataSlice.reducer,
+  [checkoutSlice.name]: checkoutSlice.reducer,
+  [cartSlice.name]: cartSlice.reducer,
+  // apis
+  [userApi.reducerPath]: userApi.reducer,
+  [itemApi.reducerPath]: itemApi.reducer,
+})
+
+const persistedReducer = persistReducer(
+  {
+    key: "root",
+    storage,
+    whitelist: [cartSlice.name],
   },
+  rootReducer
+)
+
+export const store = configureStore({
+  reducer: persistedReducer,
   // apis middleware
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }).concat(userApi.middleware).concat(itemApi.middleware),
 })
-
 setupListeners(store.dispatch)
+
+export const persistor = persistStore(store)
 
 export { useSelector, useDispatch } from "react-redux"
 export { dataSliceActions, checkoutSliceActions, cartSliceActions }
