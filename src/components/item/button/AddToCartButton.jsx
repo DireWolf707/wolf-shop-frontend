ae762856890c53ccfffd049bef0189d253d67004
DireWolf707 <rahulsood707@gmail.com>
2023-05-19 00:12:40 +0530

    added toast when add to cart

--- src/components/item/button/AddToCartButton.jsx
+++ src/components/item/button/AddToCartButton.jsx
@@ -1,11 +1,18 @@
 import { Button } from "@mui/material"
 import { useDispatch, cartSliceActions } from "../../../store"
+import { toast } from "react-hot-toast"
+import Toast from "../../layout/Toast"
 
 const AddToCartButton = ({ item }) => {
   const dispatch = useDispatch()
 
+  const addToCartHandler = () => {
+    dispatch(cartSliceActions.addItem(item))
+    toast.success(Toast(`${item.name} added to cart!`))
+  }
+
   return (
-    <Button onClick={() => dispatch(cartSliceActions.addItem(item))} variant="contained" color="primary" fullWidth>
+    <Button onClick={addToCartHandler} variant="contained" color="primary" fullWidth>
       Add To Cart
     </Button>
   )
